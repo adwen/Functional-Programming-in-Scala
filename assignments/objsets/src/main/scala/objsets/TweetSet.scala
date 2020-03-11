74f42fcdcafe4211a0838b121566cd5a759f3345
adwen <matttram@ymail.com>
2020-03-10 20:08:42 -0400

    currying union

--- assignments/objsets/src/main/scala/objsets/TweetSet.scala
+++ assignments/objsets/src/main/scala/objsets/TweetSet.scala
@@ -172,7 +172,7 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-  override def union(that: TweetSet): TweetSet = left union (right union (that incl elem))
+  override def union(that: TweetSet): TweetSet = left union right union that incl elem
 }
 
 trait TweetList {
